5a6cff65f3ca49d5a51286039317a5e0765e7035
Andy K <andykampfschulte@gmail.com>
2022-04-10 22:16:31 -0700

    Trying to get everything organised. Getting there.

---
+++ R/beamer.R
@@ -0,0 +1,44 @@
+#' Steve's Beamer Template
+#'
+#' This is the R Markdown template I use for in-class or in-person presentations. For
+#' a discussion of some of its features, see here:
+#' <http://svmiller.com/blog/2019/08/r-markdown-template-beamer-presentations/>.
+#'
+#' Note that `xelatex` is not required for this template but is heavily suggested. If
+#' you elect to use `xelatex`, you *must* supply something in `titlefont:` and
+#' `mainfont:` that your particular LaTeX installation will recognize. See discussion
+#' here: \url{https://github.com/svmiller/stevetemplates/issues/4}
+#'
+#' # About YAML header fields
+#'
+#' | FIELD  | DESCRIPTION |
+#' | ------ | ----------- |
+#' | `title` | Title of the presentation |
+#' | `subtitle` | Subtitle of the presentation. Optional, but useful. For in-class lectures, I put the class name here. |
+#' | `institute` | List your department/college here. Optional. |
+#' | `titlegraphic` | Put a path to university watermark here. Optional. |
+#' | `scaletg` | Scale `titlegraphic` relative to the width of the document. Optional, defaults to .25. |
+#' | `make149` | Logical, gives 14:9 dimension to presentation (my favorite). Optional, defaults to FALSE. |
+#' | `make169` | Logical, gives 16:9 dimension to presentation (my favorite). Optional, defaults to FALSE. |
+#' | `mainfont` | The main font to use with `xelatex`. I use `Open Sans` here. Optional, but recommended. |
+#' | `titlefont` | The font for slide titles. I use `Titillium Web` here. Optional, but recommended.  |
+#' | `primarycolor` | Specifies a "primary" color in six-digit RGB color. Defaults to `522D80`. Optional. |
+#' | `secondarycolor` | Specifies a "secondary" color in six-digit RGB color. Defaults to `F56600`. Optional. |
+#'
+#'
+#' @inheritParams rmarkdown::beamer_presentation
+#' @param ... Arguments to [`rmarkdown::beamer_presentation`].
+#' @md
+#' @export
+#'
+beamer <- function(...){
+  templ <- system.file("rmarkdown", "templates", "beamer", "resources", "template.tex", package = "SSCItemplates")
+  rmarkdown::beamer_presentation(template = templ,
+                                 ...)
+}
+
+#' @rdname beamer
+#' @export
+templ_beamer <- function(){
+  print(system.file("rmarkdown", "templates", "beamer", "resources", "template.tex", package = "SSCItemplates"))
+}
